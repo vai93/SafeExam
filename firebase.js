88f0d517559735ddaca52edc4ac768dc017f7ddb
vai93 <87764295+vai93@users.noreply.github.com>
2025-01-30 16:34:42 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";  
 const firebaseConfig = {
-  apiKey:process.env.FIREBASE_API_KEY,
-  authDomain:process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey:"AIzaSyB44lalCeugu5WDRCXS3kwsRYvAIvAS0qs",
+  authDomain:"safeexam-6b2bd.firebaseapp.com" ,
+  projectId: "safeexam-6b2bd",
   storageBucket: "safeexam-6b2bd.firebasestorage.app",
   messagingSenderId: "40444949215",
   appId: "1:40444949215:web:d239e9844c328c9acc6f6a",
