127bc2eaa0c75f04883c8d283f0b157bf3bb42b8
vai93 <87764295+vai93@users.noreply.github.com>
2025-02-13 16:14:06 +0530

    Update fetchQuiz.js

--- js/fetchQuiz.js
+++ js/fetchQuiz.js
@@ -18,7 +18,7 @@ if (rollNumber) {
 }
 async function nameFind(rollNumber) {
     try {
-        const querySnapshot = await getDocs(collection(db, "FaculyDatabase"));
+        const querySnapshot = await getDocs(collection(db, "StudentDetails2022A1"));
         let nameFound = false;
 
         querySnapshot.forEach((doc) => {
