40f0e92c31a35a2118ae90504a7bc4e0748c8142
vai93 <87764295+vai93@users.noreply.github.com>
2025-03-24 15:47:56 +0530

    Update fetchQuiz.js

--- api/fetchQuiz.js
+++ api/fetchQuiz.js
@@ -1,6 +1,6 @@
 const { db } = require("../firebase-admin-setup");
 const admin = require("firebase-admin");
-
+const cookie = require("cookie");
 module.exports = async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");  // Allow all origins
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
@@ -12,6 +12,10 @@ module.exports = async (req, res) => {
         return res.status(405).json({ message: "Method Not Allowed" });
     }
     try {
+        const cookies = cookie.parse(req.headers.cookie || "");
+        if (cookies.validStudent !== "true") {
+            return res.status(401).json({ message: "Unauthorized access" });
+        }
         const { testId } = req.body;
         if (!testId) {
             return res.status(400).json({ message: "Missing test ID" });
