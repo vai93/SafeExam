dd6dd83399a9d105538ff103e7b9b16d09e125f4
vai93 <87764295+vai93@users.noreply.github.com>
2025-03-25 22:34:20 +0530

    Update fetchQuiz.js

--- api/fetchQuiz.js
+++ api/fetchQuiz.js
@@ -2,7 +2,8 @@ const { db } = require("../firebase-admin-setup");
 const admin = require("firebase-admin");
 const cookie = require("cookie");
 module.exports = async (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://safe-exam.vercel.app/");  // Allow all origins
+    // res.setHeader("Access-Control-Allow-Origin", "https://safe-exam.vercel.app/");  // Allow all origins
+     res.setHeader("Access-Control-Allow-Origin", "*");  
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
