a799834b689887f21f3fff3adc709259d7a324c2
vai93 <87764295+vai93@users.noreply.github.com>
2025-04-10 21:57:28 +0530

    Update check-cookie.js

--- api/check-cookie.js
+++ api/check-cookie.js
@@ -1,4 +1,10 @@
+const { db } = require("../firebase-admin-setup");
+const admin = require("firebase-admin");
+const cookie = require("cookie");
 module.exports = async (req, res) => {
+     res.setHeader("Access-Control-Allow-Origin", "*");  // Allow all origins
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     const cookies = req.headers.cookie || "";
     const validStudent = cookies.split("; ").find(c => c.startsWith("validStudent="))?.split("=")[1];
     if (validStudent === "true") {
